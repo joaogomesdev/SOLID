9e9cb8b080f6a9da7db8aebaf54ec2655f16d1b0
JoJo <joaopfg.2002@gmail.com>
2020-08-23 07:41:05 +0100

    backup

---
+++ src/entities/User.ts
@@ -0,0 +1,17 @@
+import { uuid } from 'uuidv4';
+
+export class User { 
+  public readonly id: string;
+
+  public readonly nome: string;
+  public readonly email: string;
+  public readonly password: string;
+
+  constructor(props: Omit<User, 'id'> , id?: string) {
+    Object.assign(this, props);
+
+    if(!id) {
+      this.id = uuid();
+    }
+  }
+}
